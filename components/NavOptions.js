85b6361f96aff667bae8b0defc70c0e3fb2c3152
Andrés Menchaca <andresfullstack@gmail.com>
2022-08-26 03:02:17 -0300

    update - first commit

---
+++ components/NavOptions.js
@@ -0,0 +1,58 @@
+import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import tw from "tailwind-react-native-classnames";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { selectOrigin } from "../slices/navSlice";
+
+const data = [
+  {
+    id: "123",
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatsScreen",
+  },
+];
+
+const NavOptions = () => {
+  const navigation = useNavigation();
+  const origin = useSelector(selectOrigin);
+
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      horizontal
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+          disabled={!origin}
+        >
+          <View style={tw`${!origin && "opacity-20"}`}>
+            <Image
+              source={{ uri: item.image }}
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+            />
+            <Text>{item.title}</Text>
+            <Icon
+              type="antdesign"
+              name="arrowright"
+              color="white"
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+            />
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
+
+export default NavOptions;
---
+++ components/RideOptionsCard.js
@@ -0,0 +1,13 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+import tw from "tailwind-react-native-classnames";
+
+const RideOptionsCard = () => {
+  return (
+    <SafeAreaView>
+      <Text>yoo yoo</Text>
+    </SafeAreaView>
+  );
+};
+
+export default RideOptionsCard;
