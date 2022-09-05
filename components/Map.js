60d0305a0f936069b3a25faaf30c4c97bc6b5443
Andrés Menchaca <andresfullstack@gmail.com>
2022-09-04 21:32:42 -0300

    update - added matrix api

--- components/Map.js
+++ components/Map.js
@@ -2,8 +2,12 @@ import { View, Text } from "react-native";
 import React, { useRef, useEffect } from "react";
 import tw from "tailwind-react-native-classnames";
 import MapView, { Marker } from "react-native-maps";
-import { useSelector } from "react-redux";
-import { selectOrigin, selectDestination } from "../slices/navSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectOrigin,
+  selectDestination,
+  setTravelTimeInfo,
+} from "../slices/navSlice";
 import MapViewDirections from "react-native-maps-directions";
 import { GOOGLE_MAPS_APIKEY } from "@env";
 
@@ -11,6 +15,7 @@ const MapScreen = () => {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
   const mapRef = useRef(null);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (!origin || !destination) return;
@@ -20,6 +25,21 @@ const MapScreen = () => {
       edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
     });
   }, [origin, destination]);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+    const getTravelTime = async () => {
+      fetch(
+        `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=${origin.description}&destinations=${destination.description}&key=${GOOGLE_MAPS_APIKEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          dispatch(setTravelTimeInfo(data.rows[0].elements[0]));
+        });
+    };
+
+    getTravelTime();
+  }, [origin, destination, GOOGLE_MAPS_APIKEY]);
 
   return (
     <MapView
--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -9,6 +9,8 @@ import React, { useState } from "react";
 import tw from "tailwind-react-native-classnames";
 import { Icon, Image } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { selectTravelTimeInfo } from "../slices/navSlice";
 
 const data = [
   {
@@ -31,9 +33,12 @@ const data = [
   },
 ];
 
+const SURGE_CHARGE_RATE = 1.5;
+
 const RideOptionsCard = () => {
   const navigation = useNavigation();
   const [selected, setSelected] = useState(null);
+  const travelTimeInfo = useSelector(selectTravelTimeInfo);
 
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
@@ -44,7 +49,9 @@ const RideOptionsCard = () => {
         >
           <Icon name="chevron-left" type="fontawesome" />
         </TouchableOpacity>
-        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+        <Text style={tw`text-center py-5 text-xl`}>
+          Select a Ride - {travelTimeInfo?.distance?.text}
+        </Text>
       </View>
 
       <FlatList
@@ -59,20 +66,40 @@ const RideOptionsCard = () => {
           >
             <Image
               style={{
-                width: 100,
-                height: 100,
+                width: 80,
+                height: 80,
                 resizeMode: "contain",
               }}
               source={{ uri: image }}
             />
             <View style={tw`-ml-6`}>
               <Text style={tw`text-xl font-semibold`}>{title}</Text>
-              <Text>Travel time...</Text>
+              <Text>{travelTimeInfo?.duration?.text} Travel Time</Text>
             </View>
-            <Text style={tw`text-xl`}>$1000</Text>
+            <Text style={tw`text-xl`}>
+              {new Intl.NumberFormat("en-HOSSDDG", {
+                style: "currency",
+                currency: "USD",
+              }).format(
+                (travelTimeInfo?.duration.value *
+                  SURGE_CHARGE_RATE *
+                  multiplier) /
+                  100
+              )}
+            </Text>
           </TouchableOpacity>
         )}
       />
+      <View>
+        <TouchableOpacity
+          disabled={!selected}
+          style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}
+        >
+          <Text style={tw`text-center text-white text-xl`}>
+            Choose {selected?.title}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
